e31c3d07b71a31d4a5b2d935df940e3fc49a40a7
NeverCarryC <nishangzhi95@gmail.com>
2025-02-02 19:45:21 +0100

    Proceso

--- src/main/java/Raton/Raton4.java
+++ src/main/java/Raton/Raton4.java
@@ -1,4 +1,17 @@
 package Raton;
 
-public class Raton4 {
+import java.util.Random;
+
+public class Raton4 extends Thread {
+    private String nombre;
+    private Random random = new Random();
+
+    public Raton4(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void run(){
+
+    }
+
 }
